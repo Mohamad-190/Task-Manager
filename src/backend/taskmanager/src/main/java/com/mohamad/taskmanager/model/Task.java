503a0a051aaceae775659aca0fc68f73796ebf28
Mohamad Aldulemi <mohamadaldulemi@gmail.com>
2026-04-04 02:25:49 +0200

    Model done

---
+++ src/backend/taskmanager/src/main/java/com/mohamad/taskmanager/model/Task.java
@@ -0,0 +1,85 @@
+package com.mohamad.taskmanager.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import java.time.LocalDate;
+
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+    public Task() {
+    }
+
+    public Task(String title, String description, boolean completed, String priority, LocalDate dueDate) {
+    }
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int Id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+
+    private boolean completed;
+
+    private String priority;
+
+    private LocalDate dueDate;
+
+    private LocalDateTime createdAt;
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        if (title == null || title.isEmpty()) {
+            throw new IllegalArgumentException("Title darf nicht leer sein");
+        }
+        this.title = title;
+
+    }
+
+    public String getDescription() {
+        return title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+
+}
